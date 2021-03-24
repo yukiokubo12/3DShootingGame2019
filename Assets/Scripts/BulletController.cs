0b15e6035fcc7103544c61ece465e1a6cbcb2efc
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-03-25 07:54:34 +0900

    ScoreControllerをBulletPrefabへアタッチした状態

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -44,7 +44,7 @@ public class BulletController : MonoBehaviour
             playerMPSlider.value = (float)currentMp / maxMp;
             time = interval;
             bullets.transform.position = bulletPos.transform.position;
-            // Debug.Log("弾発射");
+            Debug.Log("プレイヤー弾発射");
             Vector3 force;
             force = this.gameObject.transform.forward * speed;
             bullets.GetComponent<Rigidbody>().AddForce(force);
